66c64be738a5563edff9b210cff1cb7c8c1dd7b3
wodsaegh <Wout.DeSaegher@Ugent.be>
2023-04-29 18:50:44 +0200

    added exe

---
+++ first_exercise/evaluation/evaluator.py
@@ -0,0 +1,19 @@
+from validators.checks import HtmlSuite, TestSuite, ChecklistItem, BoilerplateTestSuite, Check
+from bs4 import BeautifulSoup
+from utils.file_loaders import json_loader
+import json
+import numpy as np
+import ast
+
+
+def create_suites(content: str, solution: str) -> list[TestSuite]:
+    cvg = CVGSuite(content, solution)
+    cvg.make_item("Vergelijk het aantal codeblokken",
+                  cvg.compare_nodeslength())
+    cvg.make_item("Vergelijk het aantal pijlen", cvg.compare_edgeslength())
+    cvg.make_item("Vergelijk de nodes", cvg.correct_nodes())
+    cvg.make_item("Vergelijk de edges", cvg.correct_edges())
+    cvg.make_item("Vergelijk de stippel", cvg.correct_stippel())
+    cvg.make_item("Juiste CVG", cvg.correct_CVG())
+
+    return [cvg]
