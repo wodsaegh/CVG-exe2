f82bf007383ceff78a76a8f6babcd35faaff845a
wodsaegh <Wout.DeSaegher@Ugent.be>
2023-04-30 22:04:41 +0200

    Changed feedback and description

--- first_exercise/evaluation/evaluator.py
+++ first_exercise/evaluation/evaluator.py
@@ -8,12 +8,12 @@ import ast
 
 def create_suites(content: str, solution: str) -> list[TestSuite]:
     cvg = CVGSuite(content, solution)
-    cvg.make_item("Vergelijk het aantal codeblokken",
+    cvg.make_item("Het aantal codeblokken is juist",
                   cvg.compare_nodeslength())
-    cvg.make_item("Vergelijk het aantal pijlen", cvg.compare_edgeslength())
-    cvg.make_item("Vergelijk de nodes", cvg.correct_nodes())
-    cvg.make_item("Vergelijk de edges", cvg.correct_edges())
-    cvg.make_item("Vergelijk de stippel", cvg.correct_stippel())
-    cvg.make_item("Juiste CVG", cvg.correct_CVG())
+    cvg.make_item("Het aantal pijlen is juist", cvg.compare_edgeslength())
+    cvg.make_item("De codeblokken zijn juist ingevuld", cvg.correct_nodes())
+    cvg.make_item("De pijlen staan in de juiste richting", cvg.correct_edges())
+    cvg.make_item("Er staat een doorvalpad waar een doorvalpad moet staan en een sprong waar een sprong moet staan", cvg.correct_stippel())
+    cvg.make_item("De controleverloopgraaf is helemaal juist", cvg.correct_CVG())
 
     return [cvg]
